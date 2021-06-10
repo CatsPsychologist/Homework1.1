f5adfc64472aac03d4516a326c31ea5c561d36b2
CatsPsychologist <mainofficee@gmail.com>
2021-06-10 10:36:59 +0300

    innit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const number1 = prompt("Введите первое число");
+const number2 = prompt("Введите второе число");
+
+const result = number1 - number2;
+console.log(result);
+
+alert("Результат вычеслений :" +result);
+
+const name = prompt("Введите Ваше имя");
+const surname = prompt("Введите Вашу фамилию");
+const middleName = prompt("Введите Ваше отчество");
+
+alert("Вас зовут: " + name + " " + surname + " " +  middleName);
